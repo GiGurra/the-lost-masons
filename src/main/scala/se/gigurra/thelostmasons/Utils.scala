a598dfc1ed2815814979f11ae38a175ba17dfe00
Gunnar <vilsongunnar@gmail.com>
2016-04-08 16:36:58 +0200

    SCOREgit add src/git add src/

--- src/main/scala/se/gigurra/thelostmasons/Utils.scala
+++ src/main/scala/se/gigurra/thelostmasons/Utils.scala
@@ -1,8 +1,23 @@
 package se.gigurra.thelostmasons
 
-import se.gigurra.fingdx.lmath.Vec2
+
+import se.gigurra.fingdx.lmath.{Box, Vec2}
+
+import scala.reflect.ClassTag
 
 object Utils {
+  implicit class RichBoundingBox(box: Box) {
+    def overlap(right: Box): Boolean = {
+      right.contains(Vec2(box.left, box.bottom)) ||
+        right.contains(Vec2(box.left, box.top)) ||
+        right.contains(Vec2(box.right, box.bottom)) ||
+        right.contains(Vec2(box.right, box.top)) ||
+        box.contains(Vec2(right.left, right.bottom)) ||
+        box.contains(Vec2(right.left, right.top)) ||
+        box.contains(Vec2(right.right, right.bottom)) ||
+        box.contains(Vec2(right.right, right.top))
+    }
+  }
   def randomVector: Vec2 = Vec2(math.random - 0.5, math.random - 0.5).normalized
 
   def clamp(min: Vec2, current: Vec2, max: Vec2): Vec2 = {
@@ -12,7 +27,19 @@ object Utils {
     )
   }
 
+  def sameEntityType(e1: Entity, e2: Entity): Boolean = {
+    e1.getClass == e2.getClass
+  }
+
+
   def clamp(min: Double, current: Double, max: Double): Double = {
     math.min(math.max(current, min), max)
+  }
+
+  def asBulletOpt(e: Entity): Option[Bullet] = {
+    e match {
+      case b: Bullet => Some(b)
+      case _ => None
+    }
   }
 }
